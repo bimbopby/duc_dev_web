519a21025b02cc9c267f1e6997f73afa248d3f65
longvm2280 <longmv2280@gmail.com>
2022-09-09 15:19:53 +0700

    Design trello

--- src/App.js
+++ src/App.js
@@ -1,10 +1,161 @@
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className='App'>
-      <h1 className="hello-world">Hello World</h1>
+    <div className='duc_dev_web-master'>
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://i.pinimg.com/originals/98/d5/f5/98d5f5382740a36a45e06e29f83c7434.png" alt="abc-img" />
+            Title: ABC
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+          <img src="https://i.pinimg.com/originals/98/d5/f5/98d5f5382740a36a45e06e29f83c7434.png" alt="abc-img" />
+            Title: ABC
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://i.pinimg.com/originals/98/d5/f5/98d5f5382740a36a45e06e29f83c7434.png" alt="abc-img" />
+            Title: ABC
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          
+        </ul>
+        <footer>Add another card</footer>
+        </div> 
+        <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://i.pinimg.com/originals/98/d5/f5/98d5f5382740a36a45e06e29f83c7434.png" alt="abc-img" />
+            Title: ABC
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          
+        </ul>
+        <footer>Add another card</footer>
+        </div> 
+        <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://i.pinimg.com/originals/98/d5/f5/98d5f5382740a36a45e06e29f83c7434.png" alt="abc-img" />
+            Title: ABC
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://i.pinimg.com/originals/98/d5/f5/98d5f5382740a36a45e06e29f83c7434.png" alt="abc-img" />
+            Title: ABC
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          
+        </ul>
+        <footer>Add another card</footer>
+        </div> <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://i.pinimg.com/originals/98/d5/f5/98d5f5382740a36a45e06e29f83c7434.png" alt="abc-img" />
+            Title: ABC
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          
+        </ul>
+        <footer>Add another card</footer>
+        </div> <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://i.pinimg.com/originals/98/d5/f5/98d5f5382740a36a45e06e29f83c7434.png" alt="abc-img" />
+            Title: ABC
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          
+        </ul>
+        <footer>Add another card</footer>
+        </div> 
+        <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://i.pinimg.com/originals/98/d5/f5/98d5f5382740a36a45e06e29f83c7434.png" alt="abc-img" />
+            Title: ABC
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          
+        </ul>
+        <footer>Add another card</footer>
+        </div> 
+        <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://i.pinimg.com/originals/98/d5/f5/98d5f5382740a36a45e06e29f83c7434.png" alt="abc-img" />
+            Title: ABC
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+      </div>
+      
 
     </div>
   );
